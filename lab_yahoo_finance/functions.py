3a904af01bc7a2df7184979795f87a4fa3a427a3
Alexey Ruts <72335597+rik-code@users.noreply.github.com>
2021-12-25 16:44:46 +0000

    Adding visualization (json changes)

--- lab_yahoo_finance/functions.py
+++ lab_yahoo_finance/functions.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 from matplotlib import rcParams
 from datetime import datetime
 
-KEY = os.environ.get('API_KEY')
+KEY = os.environ.get('f652c1f2c284e4708669d034b02f30999cbc83d0')
 HOST = os.environ.get('API_HOST')
 
 symbol_string = ''
@@ -43,6 +43,77 @@ def fetchStockData(symbol: str, region: str, api_key: str, api_host: str) -> dic
     else:
         return None
 
-fetch = fetchStockData('TSLA', 'US', KEY, HOST)
-with open('yahoo.json', 'w') as f:
-    f.write(json.dumps(fetch, indent=4))
+
+def parseTimestamp(data: dict) -> list:
+    """
+    API returns a lot of daata for a stock symbol. All of it's formatted
+    as a separate series of opening and closing, high and low figures of the stock
+    during each day.
+    :param data: json response
+    :return: list of dates
+    """
+    timestamp_list = []
+    timestamp_list.extend(data['chart']['result'][0]['timestamp'])
+    timestamp_list.extend(data['chart']['result'][0]['timestamp'])
+    calendar_time = []
+    for ts in timestamp_list:
+        dt = datetime.fromtimestamp(ts)
+        calendar_time.append(dt.strftime('%d/%m/%Y'))
+    return calendar_time
+
+
+def parseValues(data: dict) -> list:
+    """
+    Extracting open and close values
+    :param data: json response
+    :return: list of values
+    """
+    valuesList = []
+    valuesList.extend(data['chart']['result'][0]['indicators']['quote'][0]['open'])
+    valuesList.extend(data['chart']['result'][0]['indicators']['quote'][0]['close'])
+
+    return valuesList
+
+
+def attachEvent(data: dict) -> list:
+    """
+    Defining open and close events in a list
+    :param data: json response
+    :return: list of open and close values
+    """
+    eventList = []
+
+    for i in range(len(data['chart']['result'][0]['timestamp'])):
+        eventList.append('open')
+    for i in range(len(data['chart']['result'][0]['timestamp'])):
+        eventList.append('close')
+
+    return eventList
+
+
+with open('yahoo.json', 'r') as j:
+    o = j.read()
+    o = json.loads(o)
+    data['Timestamp'] = parseTimestamp(o)
+    data['Values'] = parseValues(o)
+    data['Events'] = attachEvent(o)
+    df = pd.DataFrame(data)
+
+    # диаграмма
+    sns.set(style='darkgrid')
+    rcParams['figure.figsize'] = 20, 10
+    rcParams['figure.subplot.bottom'] = 0.5
+
+    ax = sns.lineplot(x='Timestamp', y='Values', hue='Events',
+                      dashes=False, markers=True, data=df, sort=False)
+    ax.set_title('Symbol' + 'TSLA')
+    plt.xticks(rotation=45,
+               horizontalalignment='right',
+               fontweight='light',
+               fontsize='xx-small')
+    plt.show()
+
+#fetch = fetchStockData('TSLA', 'US', KEY, HOST)
+#with open('yahoo.json', 'w') as f:
+#    f.write(json.dumps(fetch, indent=4))
+
